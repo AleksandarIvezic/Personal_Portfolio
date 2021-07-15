a0eb2f0093197ac57064db3cfc36d058ff12575e
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-07-15 15:46:12 +0200

    Create project js for pop-up project details view

---
+++ project.js
@@ -0,0 +1,29 @@
+let projectBtns = document.getElementsByClassName("btn-project");
+let details = document.getElementsByClassName("project-details");
+let previews = document.getElementsByClassName("project-preview");
+let closeBtns = document.getElementsByClassName("close-details");
+console.log(closeBtns[0]);
+projectBtns = Array.from(projectBtns);
+details = Array.from(details);
+previews = Array.from(previews);
+closeBtns = Array.from(closeBtns);
+projectBtns.forEach((btn) => {
+  btn.addEventListener("click", displayDetails);
+});
+closeBtns.forEach((btn) => {
+  btn.addEventListener("click", closeDetails);
+});
+
+function displayDetails(e) {
+  let index = projectBtns.indexOf(e.currentTarget);
+  details[index].style.display = "block";
+  previews[index].style.display = "none";
+  console.log(e.target);
+}
+function closeDetails(e) {
+  let index = closeBtns.indexOf(e.currentTarget);
+
+  console.log(index);
+  previews[index].style.display = "block";
+  details[index].style.display = "none";
+}
