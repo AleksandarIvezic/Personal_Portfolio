0f3dd79b2e490f3f4004bec404741eaf3e6d13e8
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-07-19 12:03:54 +0200

    Add bg of project preview using script

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ let projectBtns = document.getElementsByClassName("btn-project");
 let details = document.getElementsByClassName("project-details");
 let previews = document.getElementsByClassName("project-preview");
 let closeBtns = document.getElementsByClassName("close-details");
-console.log(closeBtns[0]);
+
 projectBtns = Array.from(projectBtns);
 details = Array.from(details);
 previews = Array.from(previews);
@@ -19,10 +19,13 @@ function displayDetails(e) {
   let index = projectBtns.indexOf(e.currentTarget);
   details[index].style.display = "block";
   previews[index].style.display = "none";
-  console.log(e.target);
 }
 function closeDetails(e) {
   let index = closeBtns.indexOf(e.currentTarget);
   previews[index].style.display = "block";
   details[index].style.display = "none";
+}
+for (let i=0; i<6; i++) {
+  previews[i].style.background = "linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 48.44%, #000000 100%), url(imgs/Project-"+i+".png)";
+  previews[i].style.backgroundSize = "cover";
 }
