4af6c4986cdf07f8b6c4b604ea73e980257e9c57
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-07-19 12:11:49 +0200

    Use script to add details project images

--- project.js
+++ project.js
@@ -2,11 +2,13 @@ let projectBtns = document.getElementsByClassName("btn-project");
 let details = document.getElementsByClassName("project-details");
 let previews = document.getElementsByClassName("project-preview");
 let closeBtns = document.getElementsByClassName("close-details");
+let images = document.getElementsByClassName("img");
 
 projectBtns = Array.from(projectBtns);
 details = Array.from(details);
 previews = Array.from(previews);
 closeBtns = Array.from(closeBtns);
+images = Array.from(images);
 
 projectBtns.forEach((btn) => {
   btn.addEventListener("click", displayDetails);
@@ -27,5 +29,7 @@ function closeDetails(e) {
 }
 for (let i=0; i<6; i++) {
   previews[i].style.background = "linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 48.44%, #000000 100%), url(imgs/Project-"+i+".png)";
+  images[i].style.background = previews[i].style.background;
   previews[i].style.backgroundSize = "cover";
+  images[i].style.backgroundSize = "cover";
 }
