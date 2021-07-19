85ec9e7d08dda2ecd90d8b116cb679a53cc2182e
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-07-19 12:33:29 +0200

    Add project headings using script

--- project.js
+++ project.js
@@ -3,12 +3,18 @@ let details = document.getElementsByClassName("project-details");
 let previews = document.getElementsByClassName("project-preview");
 let closeBtns = document.getElementsByClassName("close-details");
 let images = document.getElementsByClassName("img");
+let previewHeadings = document.getElementsByClassName("preview-heading");
+let detailsHeadings = document.getElementsByClassName("details-heading");
+let projects = ["Tic-Tac-Toe", "Cake Factory", "Group our Cakes", "Canvas","Calculator", "Bujinkan web page"];
+   
 
 projectBtns = Array.from(projectBtns);
 details = Array.from(details);
 previews = Array.from(previews);
 closeBtns = Array.from(closeBtns);
 images = Array.from(images);
+previewHeadings = Array.from(previewHeadings);
+detailsHeadings = Array.from(detailsHeadings);
 
 projectBtns.forEach((btn) => {
   btn.addEventListener("click", displayDetails);
@@ -32,4 +38,6 @@ for (let i=0; i<6; i++) {
   images[i].style.background = previews[i].style.background;
   previews[i].style.backgroundSize = "cover";
   images[i].style.backgroundSize = "cover";
+  previewHeadings[i].innerHTML = projects[i];
+  detailsHeadings[i].innerHTML = projects[i];
 }
